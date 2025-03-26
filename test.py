eec1217125fd9d99a4d5101f6f74226354c8d4c4
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-03-26 03:06:10 +0000

    Added a small test for checking if the operators stores in each Layer in each PauliPath in the Circuit were accurate

--- test.py
+++ test.py
@@ -16,11 +16,19 @@ def main():
     #test_circuit = Circuit(5,4,8,[[(1,2),(3,4)],[(0,1),(2,3)],[(1,2),(3,4)]])
     test_gate_pos = [[]]*6
     for i in range(3):
-        for j in range(0,22,2):
+        for j in range(0,12,2):
             test_gate_pos[i].append((j,j+1))
-        for j in range(1,23,2):
+        for j in range(1,13,2):
             test_gate_pos[i].append((j,j+1))
-    test_circuit = Circuit(25,7,20,test_gate_pos)
+    test_circuit = Circuit(15,7,21,test_gate_pos)
+    for pauli_path in test_circuit.pauli_paths:
+        for sibs in pauli_path.layers[5].forward_sibs.values():
+            for op in sibs:
+                print(op.operator)
+    pauli = PauliOperator(["I","R","R","I","R","R"])
+    pauli.r_to_xyz()
+    for pauli_op in pauli.xyz_paulis:
+        print(pauli_op)
     print(time.time())
 
 if __name__ == "__main__":
