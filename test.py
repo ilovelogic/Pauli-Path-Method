4b1ed8e8e06f42cf7ec9dc5c4cf02365f4eded38
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-03-26 04:38:40 +0000

    Eliminated bug that was causing PauliPaths to be generated that had a weight congifuration that could not be satisfied

--- test.py
+++ test.py
@@ -9,27 +9,31 @@ def main():
 
     print(time.time())
     test_path = PauliPath(5, [3,2,3,2],[[(1,2),(3,4)],[(0,1),(2,3)],[(1,2),(3,4)],[(3,4)]])
-    for key, sibs in test_path.all_ops[0].forward_sibs.items():
+    for key, sibs in test_path.layers[0].forward_sibs.items():
         print("Collection sorted based on {}".format(key))
         for op in sibs:
             print(op.operator)
     #test_circuit = Circuit(5,4,8,[[(1,2),(3,4)],[(0,1),(2,3)],[(1,2),(3,4)]])
-    test_gate_pos = [[]]*6
-    for i in range(3):
-        for j in range(0,12,2):
-            test_gate_pos[i].append((j,j+1))
-        for j in range(1,13,2):
-            test_gate_pos[i].append((j,j+1))
-    test_circuit = Circuit(15,7,21,test_gate_pos)
+    test_gate_pos = [[]]*4
+    for i in range(0,4,2):
+        test_gate_pos[i] = [(0,1),(2,3),(4,5)]
+        test_gate_pos[i+1] = [(1,2),(3,4),(5,6)]
+    for gate_pos in test_gate_pos:
+        print(gate_pos)
+    
+    test_circuit = Circuit(7,5,15,test_gate_pos)
     for pauli_path in test_circuit.pauli_paths:
-        for sibs in pauli_path.layers[5].forward_sibs.values():
+        print("New PauliPath!")
+        print(pauli_path.weight_combo)
+        for sibs in pauli_path.layers[1].forward_sibs.values():
+            print("New siblings!")
             for op in sibs:
                 print(op.operator)
-    pauli = PauliOperator(["I","R","R","I","R","R"])
-    pauli.r_to_xyz()
-    for pauli_op in pauli.xyz_paulis:
-        print(pauli_op)
-    print(time.time())
+    #pauli = PauliOperator(["I","R","R","I","R","R"])
+    #pauli.r_to_xyz()
+    #for pauli_op in pauli.xyz_paulis:
+        #print(pauli_op)
+    #print(time.time())
 
 if __name__ == "__main__":
     main()
