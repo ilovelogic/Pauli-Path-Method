7bc7101ea17a010d9514923e6b0cac40f845902a
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-04-08 03:56:47 +0000

    Adjusted my code so that it calls rn_to_zy instead of rnp_to_xyz wheen propagating to the last layer

--- sibling_ops.py
+++ sibling_ops.py
@@ -8,12 +8,16 @@ class SiblingOps:
     """
     Initiates a list of all PauliOperator objects that map to the same next PauliOperator object list
     """
-    def __init__(self, pauli_ops:List[PauliOperator], next_op:PauliOperator):
+    def __init__(self, pauli_ops:List[PauliOperator], prior_ops:List[PauliOperator],next_op:PauliOperator):
         
         self.pauli_ops = pauli_ops
-        self.rnp_to_xyz(next_op)
+        self.prior_ops = prior_ops
+        if next_op.next_ops == None: # next_op is in the last layer of the Pauli path
+            self.rn_to_z(next_op) # must only use 'I's and 'Z's
+        else:
+            self.rnp_to_xyz(next_op)
 
-     # Must traverse each trav from left to right. 
+    # Must traverse from left to right. 
     # If we encounter an 'R' or 'N', we can do any of 'X', 'Y', and 'Z'.
     # If we encounter a 'P', we must use the Pauli at the same index in the prior layer 
     def rnp_to_xyz(self, next_op:PauliOperator):
@@ -36,14 +40,18 @@ class SiblingOps:
             
         self.fill_in_pos(filled_n_list,n_pos_list,0,0)
 
-        next_ops = []*len(filled_n_list)
+        filled_rn_list = []*len(filled_n_list)
 
         for i in range(len(filled_n_list)):
             for j in range(3 ** len(r_pos_list)):
-                next_ops[i].append(copy.deepcopy(filled_n_list[i]))
-            self.fill_in_pos(filled_n_list[i],r_pos_list,'X',0,0)
-            self.fill_in_pos(filled_n_list[i],r_pos_list,'Y',0,3 ** (len(r_pos_list)-1))
-            self.fill_in_pos(filled_n_list[i],r_pos_list,'Z',0,2 * (3 ** (len(r_pos_list)-1)))
+                filled_rn_list[i].append(copy.deepcopy(filled_n_list[i]))
+            self.fill_in_pos(filled_rn_list[i],r_pos_list,'X',0,0)
+            self.fill_in_pos(filled_rn_list[i],r_pos_list,'Y',0,3 ** (len(r_pos_list)-1))
+            self.fill_in_pos(filled_rn_list[i],r_pos_list,'Z',0,2 * (3 ** (len(r_pos_list)-1)))
+        
+        next_ops = []
+        for i in range(len(filled_n_list)):
+            next_ops.append(SiblingOps(filled_rn_list[i]),self.pauli_ops)
 
     def fill_in_pos(self,filled_pos:List[PauliOperator],pos_list:List[int], pauli:str, index:int, start:int):
         if index == len(pos_list):
@@ -64,13 +72,14 @@ class SiblingOps:
 
 
     # The first and last Pauli operator in a Pauli path can only be a tensor of 'I's and 'Z's
-    def rn_to_z(self, cur_op:PauliOperator, prior_op:PauliOperator=None):
-        for i in range(len(cur_op.operator)):
-            if cur_op.operator[i] == 'R' or cur_op.operator[i] == 'N':
-                cur_op.operator[i] = 'Z'
-            if self.operator[i] == 'P' and prior_op != None: # only possible when we are at the last (not first) op
-                if prior_op[i] == 'Z':
-                    cur_op.operator[i] = 'Z'
+    def rn_to_z(self, next_op:PauliOperator):
+        self.next_ops = [copy.deepcopy(next_op)]
+        for i in range(len(self.next_ops[0].operator)):
+            if self.next_ops[0].operator[i] == 'R' or self.next_ops[0].operator[i] == 'N':
+                self.next_ops[0].operator[i] = 'Z'
+            if self.operator[i] == 'P':
+                if self.pauli_ops[0][i] == 'Z':
+                    self.next_ops[0].operator[i] = 'Z'
                 else:
                     return 0 # indicates the need to remove the path
         return 1 # valid path possible
