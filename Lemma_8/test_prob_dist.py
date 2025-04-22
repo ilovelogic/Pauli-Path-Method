6d1c2395a04a503ff8c54e797d30003949194323
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-04-22 15:56:20 +0000

    Corrected usage of calling Jesus' functions

--- Lemma_8/test_prob_dist.py
+++ Lemma_8/test_prob_dist.py
@@ -2,25 +2,24 @@
 import unittest
 from typing import List, Tuple, DefaultDict
 from collections import defaultdict
-from pauli_operator import PauliOperator
-from pauli_op_layer import PauliOpLayer
-from pauli_path_trav import PauliPathTrav
 from circuit_sim import CircuitSim
+from Brute_Force_RCS import circuit_utils
 from prob_dist import ProbDist
-from Brute_Force_RCS.circuit_utils import random_circuit, extract_gates_info
 
 class TestProbDist(unittest.TestCase):
+    #python -m Lemma_8.test_prob_dist
     @classmethod
     def setUpClass(self):
         self.numQubits = 4
         self.depth = 2
-        circuit = CircuitSim(self.numQubits, self.depth, [[(0,1), (2,3)], [(1,2)]]) # 1D
-        self.bruteForceQC = random_circuit(self.numQubits, self.depth) # Qiskit Representation of a random circuit.
-        gates = extract_gates_info(self.bruteForceQC)
+        circuit = CircuitSim(self.numQubits, self.numQubits, [[(0,1), (2,3)], [(1,2)]]) # 1D, keeps all paths
+        self.bruteForceQC = circuit_utils.random_circuit(self.numQubits, self.depth) # Qiskit Representation of a random circuit.
+        gates = circuit_utils.extract_gates_info(self.bruteForceQC)
         self.prob_dist = ProbDist(circuit, gates)
 
-    def test_initialization(self):
-        print()
+    def test_stat_measures(self):
+        self.assertEqual(1,self.prob_dist.tvd)
+        self.assertEqual(0,self.prob_dist.xeb)
         
 
 if __name__ == '__main__':
