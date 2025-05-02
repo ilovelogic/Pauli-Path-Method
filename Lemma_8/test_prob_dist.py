24bd774cad4c3dd05a0d3404a07d894beefec0db
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-05-02 01:49:08 +0000

    Added a one qubit test case

--- Lemma_8/test_prob_dist.py
+++ Lemma_8/test_prob_dist.py
@@ -5,7 +5,13 @@ from collections import defaultdict
 from circuit_sim import CircuitSim
 from Brute_Force_RCS import circuit_utils
 from prob_dist import ProbDist
+from Pauli_Amplitude.pauli_amplitude import compute_fourier_from_raw_inputs
+from qiskit import circuit
+
 import pdb
+
+from qiskit import QuantumCircuit 
+
 #python -m pdb your_script.py
 #break exception IndexError
 
@@ -13,12 +19,14 @@ class TestProbDist(unittest.TestCase):
     #python -m Lemma_8.test_prob_dist
     @classmethod
     def setUpClass(self):
+
+        return
         self.numQubits = 3 # must be at least 3
         self.depth = 1
-        
+
         self.C = circuit_utils.random_circuit(self.numQubits, self.depth)
 
-        self.bruteForceQC = circuit_utils.random_circuit(self.numQubits, self.depth) # Qiskit Representation of a random circuit.
+        self.bruteForceQC = self.C # Qiskit Representation of a random circuit.
         gates = circuit_utils.extract_gates_info(self.bruteForceQC)
 
         gate_pos = []
@@ -33,14 +41,50 @@ class TestProbDist(unittest.TestCase):
         
         self.prob_dist = ProbDist(circuit, gates, self.bruteForceQC)
 
-    def test_stat_measures(self):
+    #def test_stat_measures(self):
         # self.assertEqual(0,self.prob_dist.tvd)
         self.assertEqual(1,self.prob_dist.xeb)
         #self.assertEqual(0,self.prob_dist.tvd)
         print(self.prob_dist.tvd)
         self.assertEqual(1,self.prob_dist.xeb)
         print(self.prob_dist.xeb)
+
+    def test_no_depth(self):
+        self.n_qubits_no_depth(1)
+
+    def n_qubits_no_depth(self, n:int):
+        self.numQubits = n # must be at least 3
+        self.depth = 1 # we will only have identities, so it's effectively a depthless circuit
+
+        self.QC = QuantumCircuit(n)
+        for i in range(n):
+            self.QC.id(i)
+
+        self.bruteForceQC = self.QC
+
+        gates = circuit_utils.extract_gates_info(self.QC)
+
+        noise_rate = 0
+
+        # change to be every possible enumeration for length n lists
         
+        s_list = [['Z','Z']]
+        #s_list = [[['Z','Z'],['Z','Z']], [['I','Z'],['I','Z']], [['Z','I'],['Z','I']]]
+
+        total_prob = 0
+        self.probs = DefaultDict(float) # hash function mapping outcomes to their probabilities
+        for i in range(1 << self.numQubits):
+            x = format(i, f'0{self.numQubits}b') # possible outcome of the circuit, represented as a string of 1's and 0's
+            self.probs[x] = 0
+
+            for s in s_list:
+                ham_weight = 1 # total number of non-identity Paulis in s
+                # each non-identity Pauli is affected by the depolarizing noise
+                # E(ρ) := (1 − γ)ρ + γ(I/2)Tr(ρ)
+                self.probs[x] += ((1-noise_rate)**ham_weight)*compute_fourier_from_raw_inputs(gates, s, x)
+            print(f'p({x}) = {self.probs[x]}')
+            total_prob += self.probs[x]
+        print(f'Total probability sum = {total_prob}') # sum should be 1
 
 if __name__ == '__main__':
     unittest.main()
